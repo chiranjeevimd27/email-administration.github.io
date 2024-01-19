c438ece9beb231c55a3c87705941108f6c05ea70
Chiranjeevi M D <105050766+chiranjeevimd27@users.noreply.github.com>
2024-01-19 13:03:04 +0530

    Add files via upload

---
+++ EmailApp.java
@@ -0,0 +1,11 @@
+package io.github;
+
+public class EmailApp {
+	
+	public static void main(String[] args) {
+		Email eml =new Email("Chiranjeevi","MD");
+		
+		System.out.println(eml.showInfo());
+		}
+
+}
